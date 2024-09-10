c22ff40441b4cfdbaf1ca5978d55dcb3c50268d6
Guilherme Guimaraes <technestowl@gmail.com>
2024-09-10 08:45:14 -0400

    Formed basic layout. Still needs creative revision

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,25 +29,107 @@
         private void InitializeComponent()
         {
             titleLabel = new Label();
+            clearBtn = new Button();
+            divisionToggle = new RadioButton();
+            additionToggle = new RadioButton();
+            subtractToggle = new RadioButton();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            multiplyToggle = new RadioButton();
             SuspendLayout();
             // 
             // titleLabel
             // 
             titleLabel.AutoSize = true;
-            titleLabel.Location = new Point(336, 33);
+            titleLabel.Location = new Point(177, 9);
             titleLabel.Name = "titleLabel";
             titleLabel.Size = new Size(98, 20);
             titleLabel.TabIndex = 0;
             titleLabel.Text = "C# Calculator";
             // 
+            // clearBtn
+            // 
+            clearBtn.Location = new Point(198, 51);
+            clearBtn.Name = "clearBtn";
+            clearBtn.Size = new Size(44, 46);
+            clearBtn.TabIndex = 1;
+            clearBtn.Text = "CE";
+            clearBtn.UseVisualStyleBackColor = true;
+            // 
+            // divisionToggle
+            // 
+            divisionToggle.AutoSize = true;
+            divisionToggle.Location = new Point(200, 204);
+            divisionToggle.Name = "divisionToggle";
+            divisionToggle.Size = new Size(33, 24);
+            divisionToggle.TabIndex = 3;
+            divisionToggle.TabStop = true;
+            divisionToggle.Text = "/";
+            divisionToggle.UseVisualStyleBackColor = true;
+            // 
+            // additionToggle
+            // 
+            additionToggle.AutoSize = true;
+            additionToggle.Location = new Point(200, 115);
+            additionToggle.Name = "additionToggle";
+            additionToggle.Size = new Size(37, 24);
+            additionToggle.TabIndex = 4;
+            additionToggle.TabStop = true;
+            additionToggle.Text = "+";
+            additionToggle.UseVisualStyleBackColor = true;
+            // 
+            // subtractToggle
+            // 
+            subtractToggle.AutoSize = true;
+            subtractToggle.Location = new Point(200, 163);
+            subtractToggle.Name = "subtractToggle";
+            subtractToggle.Size = new Size(33, 24);
+            subtractToggle.TabIndex = 5;
+            subtractToggle.TabStop = true;
+            subtractToggle.Text = "-";
+            subtractToggle.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(87, 62);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(71, 26);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(284, 62);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(71, 26);
+            textBox2.TabIndex = 7;
+            // 
+            // multiplyToggle
+            // 
+            multiplyToggle.AutoSize = true;
+            multiplyToggle.Location = new Point(198, 253);
+            multiplyToggle.Name = "multiplyToggle";
+            multiplyToggle.Size = new Size(33, 24);
+            multiplyToggle.TabIndex = 8;
+            multiplyToggle.TabStop = true;
+            multiplyToggle.Text = "*";
+            multiplyToggle.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(470, 791);
+            Controls.Add(multiplyToggle);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(subtractToggle);
+            Controls.Add(additionToggle);
+            Controls.Add(divisionToggle);
+            Controls.Add(clearBtn);
             Controls.Add(titleLabel);
             Name = "Form1";
             Text = "C# Calculator";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +137,12 @@
         #endregion
 
         private Label titleLabel;
+        private Button clearBtn;
+        private RadioButton divisionToggle;
+        private RadioButton additionToggle;
+        private RadioButton subtractToggle;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private RadioButton multiplyToggle;
     }
 }
